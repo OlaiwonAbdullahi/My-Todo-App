fb395886992bb74b6c362d8e0210d95bf45ec574
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-18 12:12:21 +0100

    update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,11 @@
 export default function Form() {
   return (
     <div className="mt-8 flex justify-center font-titalium">
-      <input type="text" className="bg-bgColor  border rounded-l-md w-1/3" />
+      <input
+        type="text"
+        placeholder="Add a Task"
+        className="bg-bgColor  border rounded-l-md w-1/3 text-textColor p-2 "
+      />
       <button className="border border-textColor rounded-e-lg p-1 w-20">
         Add
       </button>
