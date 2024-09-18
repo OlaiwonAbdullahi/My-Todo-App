bb8f59ebb26f3e34cbee4977d1eb6ba3d1a5be34
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-18 12:04:58 +0100

    update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,10 @@
 export default function Form() {
-  return <div className=""></div>;
+  return (
+    <div className="mt-8 flex justify-center font-titalium">
+      <input type="text" className="bg-bgColor  border rounded-l-md w-1/3" />
+      <button className="border border-textColor rounded-e-lg p-1 w-20">
+        Add
+      </button>
+    </div>
+  );
 }
