8eb53d0d2dd20922213607b47dcc446ee2987dd1
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-18 11:27:38 +0100

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Logo from "../assets/Logo.svg";
+import github from "../assets/github.svg";
 export default function Header() {
   return (
-    <div className="">
+    <div className=" flex h-14 ">
       <div className="">
         <img src={Logo}></img>
       </div>
-      <div className=""></div>
+      <div className="">
+        <img src={github} alt="" className="h-12 w-12" />
+      </div>
     </div>
   );
 }
