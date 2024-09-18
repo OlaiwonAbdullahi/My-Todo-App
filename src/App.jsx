fd992d4d099eeedc680faf3bf04c8b4e18708ea9
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-18 11:51:17 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Form from "./components/Form";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className=" h-screen bg-bgColor text-textColor">
       <Header />
+      <Form />
     </div>
   );
 }
